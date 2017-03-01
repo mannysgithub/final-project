44c0ce270364d9141aec081c0a8e4d72c9858918
mannysgithub <zetino@gmail.com>
2017-02-28 23:55:38 -0800

    adding teller to gh

--- fortuneTeller/sketch.js
+++ fortuneTeller/sketch.js
@@ -1,6 +1,6 @@
 var nouns = ["truck","pie","car","baseball","potato","banana","train","trash","lamp","chair","camera","sister","brother","mother","father","dog","snow","phone","toe","butt","box","book","montana","Seattle","kayak","park","capitol","president","desk","Hollywood","mountain","bicycle","television","soccer","cake"]
 var adj = ["steep","woebegone","quaint","separate","longing","rich","majestic","dead","miscreant","free","freezing","psychedelic","light","macho","magical","brown","valuable","ordinary","homely","incredible","poor","curly","unusual","third"]
-
+var adv = []
 
 function setup() {
   
