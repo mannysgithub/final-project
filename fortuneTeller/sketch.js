14f3059378ee1c880540c7eed7012c448486785f
mannysgithub <zetino@gmail.com>
2017-02-28 23:38:12 -0800

    adding fortune teller

---
+++ fortuneTeller/sketch.js
@@ -0,0 +1,11 @@
+var nouns = ["truck","pie","car","baseball","potato","banana","train","trash","lamp","chair","camera","sister","brother","mother","father","dog","snow","phone","toe","butt","box","book","montana","Seattle","kayak","park","capitol","president","desk","Hollywood","mountain","bicycle","television","soccer","cake"]
+var adj = ["steep","woebegone","quaint","separate","longing","rich","majestic","dead","miscreant","free","freezing","psychedelic","light","macho","magical","brown","valuable","ordinary","homely","incredible","poor","curly","unusual","third"]
+
+
+function setup() {
+  
+}
+
+function draw() {
+  
+}
