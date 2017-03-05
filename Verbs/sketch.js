0197478a394f7aced10041688047e14d73a94020
mannysgithub <zetino@gmail.com>
2017-03-05 00:35:28 -0800

    adding random generator beta

--- Verbs/sketch.js
+++ Verbs/sketch.js
@@ -2,11 +2,25 @@ var nouns = ["truck","pie","car","baseball","potato","banana","train","trash","l
 var adj = ["steep","woebegone","quaint","separate","longing","rich","majestic","dead","miscreant","free","freezing","psychedelic","light","macho","magical","brown","valuable","ordinary","homely","incredible","poor","curly","unusual","third"]
 var adv = ["carefully", "adoringly", "competitively", "eagerly", "really", "quietly", "loyally", " quickly", ""]
 var verbs = ["admire", "annoy", "avoid", "beg", "bend", "care", "confide", "deceive", "dare", "envy", "exaggerate", "fear", "fix", "feel", "guess", "hand", "help", "hurry", "inspire", "interfere", "listen", "learn", "mail", "meet", "melt", "play", "promise", "pretend", "race", "recognize", "risk", "rush", "stop", "surprise", "tell", "twist", "understand", "utter", "warn", "whisper", "worry", "yell"]
+var nounPPT = nouns[Math.floor(Math.random()*nouns.length)];
+//var adjD = adj[Math.floor(Math.random()*adj.length)];  
+
+
+x = 300
 
 function setup() {
-  
+  background(255);
+  createCanvas(600,600);
 }
 
 function draw() {
+  noFill();
+  ellipse(x,300, 150,150);
   
+
+  noStroke();
+  fill(120);
+  textSize(20);
+  text(nounPPT, x, 300);
+  //text(adjD, x + 50, 300)
 }
